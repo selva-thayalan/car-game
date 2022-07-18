f3df0996b1eee3e8bd66d9dda280449b7713db9e
selva-thayalan <selvathayalan.r@gmail.com>
2022-07-18 23:42:49 +0530

    Implemented moving the car to any directions when key pressed with some speed.

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ window.onload = () => {
         gameConsoleRect = document.getElementById("game-console").getClientRects()[0],
         leftPos = 0,
         topPos = 0;
+    var pressedKeys = [], carMoveTO;
 
     const LeftBound = 0,
         RightBound = gameConsoleRect.width - car.offsetWidth,
@@ -17,25 +18,38 @@ window.onload = () => {
     car.style.top = topPos + "px";
     car.style.left = leftPos + "px";
 
-    document.body.addEventListener("keydown", function (ev) {
-        switch (ev.keyCode) {
-            case UpKey:
-                topPos = Math.max(TopBound, topPos - 5);
-                car.style.top = topPos + "px";
-                break;
-            case DownKey:
-                topPos = Math.min(BottomBound, topPos + 5);
-                car.style.top = topPos + "px";
-                break;
-            case RightKey:
-                leftPos = Math.min(RightBound, leftPos + 5);
-                car.style.left = leftPos + "px";
-                break;
-            case LeftKey:
-                leftPos = Math.max(LeftBound, leftPos - 5);
-                car.style.left = leftPos + "px";
-                break;
+    carMove = () => {
+        if(pressedKeys.includes(UpKey)){
+            topPos = Math.max(TopBound, topPos - 5);
+            car.style.top = topPos + "px";
         }
-    })
+        if(pressedKeys.includes(DownKey)){
+            topPos = Math.min(BottomBound, topPos + 5);
+            car.style.top = topPos + "px";
+        }
+        if(pressedKeys.includes(RightKey)){
+            leftPos = Math.min(RightBound, leftPos + 5);
+            car.style.left = leftPos + "px";
+        }
+        if(pressedKeys.includes(LeftKey)){
+            leftPos = Math.max(LeftBound, leftPos - 5);
+            car.style.left = leftPos + "px";
+        }
+        carMoveTO = setTimeout(carMove, 15);
+    }
+    document.body.addEventListener("keyup", function (ev) {
+        let index = pressedKeys.indexOf(ev.keyCode);
+        pressedKeys.splice(index, 1);//If the pressed key is released then we need to remove that from this array.
+        if(pressedKeys.length == 0){//If no key is pressed at the end then we need to stop the infinite running function.
+            clearTimeout(carMoveTO);
+            carMoveTO = undefined;
+        }
+    });
+    document.body.addEventListener("keydown", function (ev) {
+        if (!pressedKeys.includes(ev.keyCode))
+            pressedKeys.push(ev.keyCode);
+        if(carMoveTO == undefined)
+            carMove();
+    });
 }
 
